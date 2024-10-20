4170bfe8210c61fb6480a66f49f1a57a177c39d7
Paweł Staniul <pstaniul@gmail.com>
2024-10-20 16:21:33 +0200

    Dodano treść i logikę do komponentu login

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+
+import { InsertFlyDataComponent } from '../insert-fly-data/insert-fly-data.component';
+import { RouterModule } from '@angular/router';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    InsertFlyDataComponent,
+    
+    MatCardModule,
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+
+  ngOnInit() {
+    this.performGsapAnimation();
+  }
+
+  performGsapAnimation(): void {
+    const tl = gsap.timeline({ defaults: { ease: 'power3.out', duration: 1 } });
+
+    // Animacja dla nagłówka
+    tl.from('.main-card', { y: -100, opacity: 0 });
+  }
 
 }
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButton,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressBarModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit, AfterViewInit {
+
+  isLoading: boolean = false;
+  sendingMail: boolean = false;
+  errorMessage: string | null = null;
+  loginForm!: FormGroup;
+
+  // Referencja do pola input dla userName
+  @ViewChild('emailInput') emailInput!: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.initialeLoginForm();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.emailInput.nativeElement.focus();
+      gsap.from('.loginForm', { duration: 0.5, y: -100, opacity: 0, stagger: 0.2 });
+    });
+  }
+
+  initialeLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+        this.sendingMail = true;
+      }, 2000);
+      const loginData = this.loginForm.value;
+      this.authService.login(loginData.username, loginData.password).subscribe({
+        next: () => {
+          this.errorMessage = null;
+          // Przekierowanie obsługiwane w serwisie
+        },
+        error: (message) => {
+          this.isLoading = false;
+          this.sendingMail = false;
+          this.errorMessage = message;
+        }
+      });
+    }
+  }
 
 }
