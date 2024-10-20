8d3355bfb3a498be9e6120bb6387ca2a3fff1a99
Paweł Staniul <pstaniul@gmail.com>
2024-10-20 15:33:32 +0200

    Skonfigurowano plik app.config.ts i angular.json. Dodano treść do komponentów navbar i footer

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    importProvidersFrom(ReactiveFormsModule), provideAnimationsAsync()
+  ]
 };
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButton,
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent implements OnInit {
+
+  userName: string | null = null;
+
+  constructor(
+    public authService: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
+      this.initializeNavbar();
+    }
+
+    this.authService.loginSuccess$.subscribe(() => {
+      this.initializeNavbar();
+    });
+  }
+
+  initializeNavbar(): void {
+    this.userName = this.authService.getName();
+  }
 }
